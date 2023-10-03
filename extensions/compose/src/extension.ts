a4fc8ddeee5d89bc73434aa309f002cf4e15d3c1
Charlie Drage <charlie@charliedrage.com>
2023-10-03 14:55:45 -0400

    feat: add install system-wide preference button for compose extension
    ### What does this PR do?
    
    * Add a "toggle" button for installing compose system wide
    * Adds the first initial compose onboarding sequence which will only appear when compose isn't installed system wide (for now, as that's the only functionality at the moment / setting).
    * Button will disappear / after it has been installed system wide.
    
    ### Screenshot/screencast of this PR
    
    <!-- Please include a screenshot or a screencast
    explaining what is doing this PR -->
    
    https://github.com/containers/podman-desktop/assets/6422176/3535f0b1-48ae-49c9-b5d0-696e93389f5f
    
    ### What issues does this PR fix or reference?
    
    <!-- Include any related issues from Podman Desktop
    repository (or from another issue tracker). -->
    
    Closes https://github.com/containers/podman-desktop/issues/3810
    
    ### How to test this PR?
    
    <!-- Please explain steps to reproduce -->
    
    1. Type `whereis docker-compose` and delete it (most likely
       /usr/local/bin/docker-compose.
    2. Make sure you have the binary still in
       .local/share/containers/podman-desktop
    3. Click on onboarding for Compose
    4. Press the toggle button for system-wide install.
    
    Signed-off-by: Charlie Drage <charlie@charliedrage.com>

--- extensions/compose/src/extension.ts
+++ extensions/compose/src/extension.ts
@@ -17,22 +17,55 @@
  ***********************************************************************/
 
 import { Octokit } from '@octokit/rest';
-import type * as extensionApi from '@podman-desktop/api';
+import * as extensionApi from '@podman-desktop/api';
 import { Detect } from './detect';
 import { ComposeExtension } from './compose-extension';
 import { ComposeGitHubReleases } from './compose-github-releases';
 import { OS } from './os';
 import { ComposeWrapperGenerator } from './compose-wrapper-generator';
 import * as path from 'path';
+import * as handler from './handler';
+
 let composeExtension: ComposeExtension | undefined;
 
 export async function activate(extensionContext: extensionApi.ExtensionContext): Promise<void> {
-  // do not hold the activation promise
+  // Post activation
   setTimeout(() => {
     postActivate(extensionContext).catch((error: unknown) => {
       console.error('Error activating extension', error);
     });
   }, 0);
+
+  // Check docker-compose binary has been installed and update both
+  // the configuration setting and the context accordingly
+  await handler.updateConfigAndContextComposeBinary(extensionContext);
+
+  // Setup configuration changes if the user toggles the "Install compose system-wide" boolean
+  handler.handleConfigurationChanges(extensionContext);
+
+  // Update the status based upon if the binary is installed SYSTEM WIDE or not
+  // we will still show "Setup.." if the user has not installed the binary system-wide
+  // as `podman compose` is unable to be used unless it's installed system-wide
+  let status: extensionApi.ProviderStatus = 'not-installed';
+  if (await handler.isDockerComposeInstalledSystemWide(extensionContext)) {
+    status = 'installed';
+  }
+
+  // Need to "ADD" a provider so we can actually press the button!
+  // We set this to "unknown" so it does not appear on the dashboard (we only want it in preferences).
+  const providerOptions: extensionApi.ProviderOptions = {
+    name: 'Compose',
+    id: 'Compose',
+    status: status,
+    images: {
+      icon: './icon.png',
+    },
+  };
+
+  providerOptions.emptyConnectionMarkdownDescription = `Compose is a specification for defining and running multi-container applications. We support both [podman compose](https://docs.podman.io/en/latest/markdown/podman-compose.1.html) and [docker compose](https://github.com/docker/compose) commands.\n\nMore information: [compose-spec.io](https://compose-spec.io/)`;
+
+  const provider = extensionApi.provider.createProvider(providerOptions);
+  extensionContext.subscriptions.push(provider);
 }
 
 async function postActivate(extensionContext: extensionApi.ExtensionContext): Promise<void> {
