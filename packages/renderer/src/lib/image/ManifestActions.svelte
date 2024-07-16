69e8d388161a4690c7edc379d1af49de9f67d02e
Charlie Drage <charlie@charliedrage.com>
2024-07-16 10:47:31 -0400

    render

---
+++ packages/renderer/src/lib/image/ManifestActions.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
+
+import ListItemButtonIcon from '../ui/ListItemButtonIcon.svelte';
+import ActionsWrapper from './ActionsMenu.svelte';
+import type { ImageInfoUI } from './ImageInfoUI';
+
+export let onPushManifest: (imageInfo: ImageInfoUI) => void;
+export let image: ImageInfoUI;
+export let dropdownMenu = false;
+export let detailed = false;
+
+async function pushManifest(imageInfo: ImageInfoUI): Promise<void> {
+  onPushManifest(imageInfo);
+}
+</script>
+
+<!-- If dropdownMenu is true, use it, otherwise just show the regular buttons -->
+<ActionsWrapper dropdownMenu="{dropdownMenu}">
+  <ListItemButtonIcon
+    title="Push Image"
+    onClick="{() => pushManifest(image)}"
+    menu="{dropdownMenu}"
+    detailed="{detailed}"
+    icon="{faArrowUp}" />
+</ActionsWrapper>
